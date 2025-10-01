225d400714e8b31062de7847ffd78c8deedefecc
ajiramandani111-ux <ajiramandani111@gmail.com>
2025-10-01 20:05:44 +0700

    ModifikasiPercobaan2.

--- jobsheet5/ModifikasiPercobaan2.java
+++ jobsheet5/ModifikasiPercobaan2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ifElseCetakKRS15 {
+public class ModifikasiPercobaan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
